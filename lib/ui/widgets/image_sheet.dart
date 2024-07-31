ab9e18024324558edde17396cd795f0b98b52edc
debuggerdev24 <debuggerdev.dds@gmail.com>
2024-07-31 13:42:45 +0530

    updated download functionality

--- lib/ui/widgets/image_sheet.dart
+++ lib/ui/widgets/image_sheet.dart
@@ -1,13 +1,18 @@
 import 'dart:developer';
 import 'dart:io';
+import 'dart:typed_data';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gallery_saver_plus/gallery_saver.dart';
 import 'package:go_router/go_router.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:photo_view/photo_view.dart';
+import 'package:shopease_app_flutter/services/base_api_service.dart';
 import 'package:shopease_app_flutter/utils/app_colors.dart';
 import 'package:shopease_app_flutter/utils/extensions/context_ext.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 
 Future<void> showImageSheet({
   required BuildContext context,
@@ -65,12 +70,13 @@ Future<void> showImageSheet({
                         IconButton.filled(
                           onPressed: () async {
                             debugPrint("URL: - $imgUrl");
-                            if (await canLaunchUrlString(imgUrl)) {
-                              bool isDone = await launchUrlString(imgUrl);
-                              if (isDone && context.mounted) {
-                                context.showSuccessMessage("Downloaded");
-                              }
-                            }
+                            await _saveNetworkImage(imgUrl, context);
+                            // if (await canLaunchUrlString(imgUrl)) {
+                            //   bool isDone = await launchUrlString(imgUrl);
+                            //   if (isDone && context.mounted) {
+                            //     context.showSuccessMessage("Downloaded");
+                            //   }
+                            // }
                           },
                           style: IconButton.styleFrom(
                             backgroundColor: AppColors.lightGreyColor,
@@ -85,4 +91,39 @@ Future<void> showImageSheet({
           ),
         );
       });
+}
+
+_saveNetworkImage(String url, BuildContext context) async {
+  try {
+    var response = await Dio().get(
+      url,
+      options: Options(responseType: ResponseType.bytes),
+    );
+
+    // final result = await GallerySaver.saveImage(
+    //   Uint8List.fromList(response.data),
+    //   quality: 60,
+    //   name: '${DateTime.now().millisecondsSinceEpoch}',
+    // );
+    // log(result);
+
+    final dir = await getTemporaryDirectory();
+    // Create an image name
+    var filename = '${dir.path}/image.png';
+    // Save to filesystem
+    final file = File(filename);
+    await file.writeAsBytes(response.data);
+
+    await GallerySaver.saveImage(file.path, albumName: 'Shopease')
+        .then((bool? success) {
+      log('success => $success');
+      if (success == true) {
+        context.showSuccessMessage('Downloaded successfully.');
+      } else {
+        context.showErrorMessage('Something went wrong.');
+      }
+    });
+  } catch (e) {
+    log('error while downloading => $e');
+  }
 }
